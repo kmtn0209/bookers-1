ad123919686471cb013b4818c7a28719cda35fe3
EC2 Default User <ec2-user@ip-172-31-30-6.ap-northeast-1.compute.internal>
2023-08-14 15:58:53 +0000

    [midle]3-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
  get "/" => 'books#top'
- get "/books" => 'books#new'
- post 'books' => 'books#create'
- get 'books/:id' => 'books#show', as: 'list'
- get 'books/:id/edit' => 'books#edit', as: 'edit_list'
+ get "/books" => 'books#new',as: 'new'
+ post 'books' => 'books#create', as: 'create'
+ get 'books/:id' => 'books#show', as: 'book'
+ get 'books/:id/edit' => 'books#edit', as: 'edit_book'
  patch 'books/:id' => 'books#update', as: 'update_book'
+ delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
